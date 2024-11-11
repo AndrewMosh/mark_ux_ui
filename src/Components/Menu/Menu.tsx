b8a4997d6467bbc9f26ede5a03a657db668eaf16
AndrewMosh <malinaenglishclub@gmail.com>
2024-11-11 17:59:51 +0300

    feat:unread message added

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -3,41 +3,44 @@ import "./Menu.scss";
 import { MenuData } from "../../utils/MenuData";
 import { useState, useEffect } from "react";
 import { useToggleMenuStore } from "../../store/useToggleMenuStore";
+import { usePortfolioStore } from "../../store/usePortfolioStore";
 
 export const Menu = () => {
-  const location = useLocation(); // Получаем текущий роут
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
-  const {toggleMenu} = useToggleMenuStore();
+    const location = useLocation(); // Получаем текущий роут
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+    const { toggleMenu } = useToggleMenuStore();
+    const { readMessage, newMessage } = usePortfolioStore();
 
-  useEffect(() => {
-    // При изменении роута обновляем активный элемент
-    const currentIndex = MenuData.findIndex(item => item.link === location.pathname);
-    if (currentIndex !== -1) {
-      setActiveIndex(currentIndex);
-    }
-  }, [location]);
+    useEffect(() => {
+        // При изменении роута обновляем активный элемент
+        const currentIndex = MenuData.findIndex((item) => item.link === location.pathname);
+        if (currentIndex !== -1) {
+            setActiveIndex(currentIndex);
+        }
+    }, [location]);
 
-  return (
-    <div className="menu">
-      {MenuData.map((item, index) => (
-        <Link
-          key={index}
-          to={item.link}
-          className='menu__item'
-          onClick={() => {
-			setActiveIndex(index)
-			toggleMenu();
-		  } }
-        >
-          <button
-            className={`menu__button ${activeIndex === index ? "menu__button--active" : ""}`}
-          >
-            <img className="menu__img" src={item.img} alt={item.title} />
-            <p className="menu__title">{item.title}</p>
-          </button>
-        </Link>
-      ))}
-    </div>
-  );
+    return (
+        <div className="menu">
+            {MenuData.map((item, index) => (
+                <Link
+                    key={index}
+                    to={item.link}
+                    className="menu__item"
+                    onClick={() => {
+                        setActiveIndex(index);
+                        toggleMenu();
+                        if (item.title === "Портфолио") readMessage();
+                    }}
+                >
+                    <button className={`menu__button ${activeIndex === index ? "menu__button--active" : ""}`}>
+                        <img className="menu__img" src={item.img} alt={item.title} />
+                        <p className="menu__title">
+                            {item.title}
+                            {item.title === "Портфолио" && newMessage === "1" ? <span className="menu__unread">{newMessage}</span> : null}
+                        </p>
+                    </button>
+                </Link>
+            ))}
+        </div>
+    );
 };
-
--- src/store/usePortfolioStore.ts
+++ src/store/usePortfolioStore.ts
@@ -4,16 +4,19 @@ import { PortfolioData, PortfolioItem } from "../utils/PortfolioData";
 
 interface PortfolioState {
 	portfolio: PortfolioItem[];
+	newMessage: string | null;
 	filterType: string;
 	setFilterType: (type: string) => void;
 	filteredPortfolio: PortfolioItem[];
 	filterPortfolio: () => void;
+	readMessage: () => void
   }
 
 const portfolioData = PortfolioData.list
 
 export const usePortfolioStore = create<PortfolioState>((set) => ({
 	portfolio: portfolioData,
+	newMessage:'1',
 	filterType: 'all', 
 	setFilterType: (type: string) => set({ filterType: type }),
 	
@@ -23,5 +26,9 @@ export const usePortfolioStore = create<PortfolioState>((set) => ({
 		filteredPortfolio: state.filterType === 'all'
 		  ? state.portfolio
 		  : state.portfolio.filter(item => item.type === state.filterType)
+	  })),
+	readMessage: () =>
+	  set(() => ({
+		newMessage: null
 	  })),
   }));
